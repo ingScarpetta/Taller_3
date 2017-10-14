636197712b3cefe65ddb3bafc511c95f7cb5e43e
Estudiantes <Estudiantes@labing50508.udistrital.edu.co>
2017-10-14 17:47:34 -0500

    llamando el web service  desde un cliente

--- CalculatorWS_Client_Application/src/calculatorws_client_application/CalculatorWS_Client_Application.java
+++ CalculatorWS_Client_Application/src/calculatorws_client_application/CalculatorWS_Client_Application.java
@@ -16,6 +16,20 @@ public class CalculatorWS_Client_Application {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+        int i = 3;
+        int j = 4;
+        int result = add(i, j);
+        System.out.println("Result = " + result);
+    } catch (Exception ex) {
+        System.out.println("Exception: " + ex);
+    }
+    }
+
+    private static int add(int i, int j) {
+        org.me.calculator.CalculatorWS_Service service = new org.me.calculator.CalculatorWS_Service();
+        org.me.calculator.CalculatorWS port = service.getCalculatorWSPort();
+        return port.add(i, j);
     }
     
 }
